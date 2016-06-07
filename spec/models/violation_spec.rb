fcf99256654079ea96758505b3a0245b5b04a438
July Tran <julyytran@gmail.com>
2016-06-06 20:16:09 -0600

    validations for model

--- spec/models/violation_spec.rb
+++ spec/models/violation_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Violation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :violation_id }
+  it { should validate_presence_of :inspection_id }
+  it { should validate_presence_of :violation_category }
+  it { should validate_presence_of :violation_date }
+  it { should validate_presence_of :violation_type }
 end
