0c7151b910fbfd6dd8ee4bc8dd401cd130f29900
July Tran <julyytran@gmail.com>
2016-06-07 18:04:02 -0600

    correct data

--- app/assets/javascripts/categories-chart.js
+++ app/assets/javascripts/categories-chart.js
@@ -1,39 +1,32 @@
-$(document).ready(function(){
-  loadData();
-});
-
-let ctx = document.getElementById("chart");
-
-function loadData() {
-  $.getJSON("/api/v1/categories", function (categories) {
-    createChart(ctx, categories);
-  });
-}
-
 function createChart(ctx, categories) {
   new Chart(ctx, {
     type: 'bar',
     data: {
       labels: categoryNames(categories),
       datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        label: 'Number of Violations',
+        data: categoryCounts(categories),
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
           'rgba(54, 162, 235, 0.2)',
           'rgba(255, 206, 86, 0.2)',
           'rgba(75, 192, 192, 0.2)',
           'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
         ],
         borderColor: [
-          'rgba(255,99,132,1)',
+          'rgba(255, 99, 132, 1)',
           'rgba(54, 162, 235, 1)',
           'rgba(255, 206, 86, 1)',
           'rgba(75, 192, 192, 1)',
           'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',        ],
         borderWidth: 1
       }]
     },
@@ -55,4 +48,12 @@ function categoryNames(categories) {
     names.push(categories[i].name);
   }
   return names;
+}
+
+function categoryCounts(categories) {
+  let counts = [];
+  for (var i = 0; i < categories.length; i++) {
+    counts.push(categories[i].count);
+  }
+  return counts;
 }
---
+++ app/assets/javascripts/load-data.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  loadData();
+});
+
+let ctx = document.getElementById("chart");
+
+function loadData() {
+  $.getJSON("/api/v1/categories", function (categories) {
+    createChart(ctx, categories);
+  });
+}
