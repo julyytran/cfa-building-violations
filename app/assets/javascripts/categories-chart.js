02d0491b8884709c8f4f203306a1d05cbf452901
July Tran <julyytran@gmail.com>
2016-06-07 18:28:35 -0600

    some styling

--- app/assets/javascripts/categories-chart.js
+++ app/assets/javascripts/categories-chart.js
@@ -26,8 +26,10 @@ function createChart(ctx, categories) {
           'rgba(255, 99, 132, 1)',
           'rgba(54, 162, 235, 1)',
           'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',        ],
-        borderWidth: 1
+          'rgba(75, 192, 192, 1)',
+        ],
+        borderWidth: 1,
+        borderSkipped: 'right'
       }]
     },
     options: {
