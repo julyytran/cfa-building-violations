fbae4dc22b01cdab599413f1dbdcc393a511bebc
July Tran <julyytran@gmail.com>
2016-06-12 21:28:39 -0600

    displaying correct data

--- app/assets/javascripts/violations_timesheet.js
+++ app/assets/javascripts/violations_timesheet.js
@@ -1,15 +1,17 @@
 function createTimeline(categories) {
-  new Timesheet('timesheet', 2002, 2013, [
-    ['9/2002', 'A freaking awesome time', 'lorem'],
-    ['06/2002', '09/2003', 'Some great memories', 'ipsum'],
-    ['2003', 'Had very bad luck'],
-    ['10/2003', '2006', 'At least had fun', 'dolor'],
-    ['02/2005', '05/2006', 'Enjoyed those times as well', 'ipsum'],
-    ['07/2005', '09/2005', 'Bad luck again', 'default'],
-    ['10/2005', '2008', 'For a long time nothing happened', 'dolor'],
-    ['01/2008', '05/2009', 'LOST Season #4', 'lorem'],
-    ['01/2009', '05/2009', 'LOST Season #4', 'lorem'],
-    ['02/2010', '05/2010', 'LOST Season #5', 'lorem'],
-    ['09/2008', '06/2010', 'FRINGE #1 & #2', 'ipsum']
-  ]);
+  var timesheetData = createTimesheet(categories);
+  new Timesheet('timesheet', 2012, 2012, timesheetData);
+}
+
+function createTimesheet (categories) {
+  let timesheetContents = [];
+  for (var i = 0; i < categories.length; i++) {
+    if (categories[i].count === 1) {
+      timesheetContents.push([categories[i].earliest_violation_month + '/2012', categories[i].name + " - Only Violation"]);
+    } else {
+      timesheetContents.push([categories[i].earliest_violation_month + '/2012', categories[i].name + " - Earliest Violation"]);
+      timesheetContents.push([categories[i].latest_violation_month + '/2012', categories[i].name + " - Latest Violation"]);
+    }
+  }
+  return timesheetContents;
 }
--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -2,10 +2,10 @@ class CategorySerializer < ActiveModel::Serializer
   attributes :name, :count, :latest_violation_month, :earliest_violation_month
 
   def earliest_violation_month
-    self.object.earliest_violation_date.month
+    self.object.earliest_violation_date.month.to_s
   end
 
   def latest_violation_month
-    self.object.latest_violation_date.month
+    self.object.latest_violation_date.month.to_s
   end
 end
