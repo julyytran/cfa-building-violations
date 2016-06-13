ec5825612c81b70bab60fb38b6885c7bf671c9ab
July Tran <julyytran@gmail.com>
2016-06-12 21:15:22 -0600

    formate violations dates into datetime and serialize

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,3 +1,11 @@
 class CategorySerializer < ActiveModel::Serializer
-  attributes :name, :count, :earliest_violation_date, :latest_violation_date
+  attributes :name, :count, :latest_violation_month, :earliest_violation_month
+
+  def earliest_violation_month
+    self.object.earliest_violation_date.month
+  end
+
+  def latest_violation_month
+    self.object.latest_violation_date.month
+  end
 end
--- db/migrate/20160607231520_create_categories.rb
+++ db/migrate/20160607231520_create_categories.rb
@@ -5,8 +5,8 @@ class CreateCategories < ActiveRecord::Migration
     create_table :categories do |t|
       t.integer :count
       t.citext :name
-      t.string :earliest_violation_date
-      t.string :latest_violation_date
+      t.datetime :earliest_violation_date
+      t.datetime :latest_violation_date
 
       t.timestamps null: false
     end
